8205a35a64441bc9f7ee64e0c3c3095c2cbfc0e1
Brunno Israel <105972819+brunnoisrael@users.noreply.github.com>
2026-03-19 15:09:21 -0300

    feat: Inclusão de autenticação de token via cabeçalho na bp prompt

--- api/routes.py
+++ api/routes.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, jsonify, request
-from flask_login import login_required, login_manager
 from services.prompt_service import workload
 import os
 
 prompt_bp = Blueprint("prompt", __name__)
 
 @prompt_bp.route("/prompt", methods=["POST"])
-@login_required
 def prompt():
     header_auth = request.headers.get("Authorization")
+    if not header_auth:
+        return jsonify({"error": "Ausente Authorization no Header"}), 401
     data = request.json
     prompt = data.get("prompt")
     resultado = workload(prompt)
