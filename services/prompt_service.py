43155e20aa92ab1b50c7298cb3e4d85631ab9d35
Brunno Israel <105972819+brunnoisrael@users.noreply.github.com>
2026-03-18 23:07:15 -0300

    alteração: comentários de objetivo do arquivo prompt_service.py

--- services/prompt_service.py
+++ services/prompt_service.py
@@ -1,6 +1,12 @@
 from router.prompt_classifier import decider
 from router.model_router import gateway
 
+"""
+    1. Efetua a chamada (prompt_classifier.py) da SLM para definir qual categoria o prompt do usuario se enquadra
+    2. Efetua a chamada (model_router.py) para chamar a LLM da categoria definida para responder o prompt.
+    
+"""
+
 def workload(prompt):
     classified_prompt = decider(prompt)
     resultado_workload = gateway(classified_prompt)
