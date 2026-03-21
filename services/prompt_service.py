153b49e82872fea79193cebde418241b9a5d670c
Brunno Israel <105972819+brunnoisrael@users.noreply.github.com>
2026-03-21 15:02:33 -0300

    feat: finalização das chamadas das funções da estrutura

--- services/prompt_service.py
+++ services/prompt_service.py
@@ -1,5 +1,6 @@
 from router.prompt_classifier import decider
 from router.model_router import gateway
+from llm.ollama_client import OllamaClient
 
 """
     1. Efetua a chamada (prompt_classifier.py) da SLM para definir qual categoria o prompt do usuario se enquadra
@@ -9,5 +10,6 @@ from router.model_router import gateway
 
 def workload(prompt):
     classified_prompt = decider(prompt)
-    resultado_workload = gateway(classified_prompt)
-    return resultado_workload
+    result_workload = gateway(classified_prompt)
+    response_llm = OllamaClient.ollama_llm(result_workload)
+    return response_llm
