4692d9284baa05a69c5c127037c101104d12e05a
Brunno Israel <105972819+brunnoisrael@users.noreply.github.com>
2026-03-21 15:05:49 -0300

    docs: adição de comentários no código

--- api/routes.py
+++ api/routes.py
@@ -6,10 +6,19 @@ prompt_bp = Blueprint("prompt", __name__)
 
 @prompt_bp.route("/prompt", methods=["POST"])
 def prompt():
+
+    # Solicita TOKEN de autorização no header
     header_auth = request.headers.get("Authorization")
+
+    # Verifica se o header_auth (autorização no cabeçalho) foi passado
     if not header_auth:
         return jsonify({"error": "Ausente Authorization no Header"}), 401
+    
     data = request.json
+
+    # Solicita "prompt:" no body
     prompt = data.get("prompt")
+
+    # Passa o prompt para o prompt_service orquestrar as chamadas das SLMs e LLMs
     resultado = workload(prompt)
     return resultado
