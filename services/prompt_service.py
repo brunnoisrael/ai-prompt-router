af06209fd411dd4e896c37daeb7b77862b720cbd
Brunno Israel <105972819+brunnoisrael@users.noreply.github.com>
2026-03-21 17:53:04 -0300

    feat: inclusão de golden sinals nas requisições para o prometheus (metrics.py)

--- services/prompt_service.py
+++ services/prompt_service.py
@@ -1,6 +1,8 @@
+import time
 from router.prompt_classifier import decider
 from router.model_router import gateway
 from llm.ollama_client import OllamaClient
+from observability.metrics import Count_requests, Response_time, Errors_requests
 
 """
     1. Efetua a chamada (prompt_classifier.py) da SLM para definir qual categoria o prompt do usuario se enquadra
@@ -9,8 +11,30 @@ from llm.ollama_client import OllamaClient
 """
 
 def workload(prompt):
+
     classified_prompt = decider(prompt)
     result_workload = gateway(classified_prompt)
     client = OllamaClient()
-    response_llm = client.ollama_llm(model=result_workload, prompt=prompt)
-    return response_llm
+
+    # Horário de inicio da requisição
+    start_time = time.time()
+
+    try:
+        response_llm = client.ollama_llm(model=result_workload, prompt=prompt)
+
+        Count_requests.labels(model=result_workload).inc()
+
+        return response_llm
+    
+    except:
+
+        Errors_requests.labels(model=result_workload).inc()
+        raise
+
+    finally:
+        
+        # Duração da requisição
+        duration = time.time() - start_time
+        Response_time.labels(model=result_workload).observe(duration)
+
+    
