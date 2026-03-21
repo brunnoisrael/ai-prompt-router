752b82ea8aeb625b83646d1147c434427e997cda
Brunno Israel <105972819+brunnoisrael@users.noreply.github.com>
2026-03-21 18:06:30 -0300

    feat: inclusão de rota de métricas para o prometheus

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, Response
 from services.prompt_service import workload
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 import os
 
+# Registra a Blueprint da rota de prompt (POST)
 prompt_bp = Blueprint("prompt", __name__)
 
 @prompt_bp.route("/prompt", methods=["POST"])
@@ -22,3 +24,10 @@ def prompt():
     # Passa o prompt para o prompt_service orquestrar as chamadas das SLMs e LLMs
     resultado = workload(prompt)
     return resultado
+
+# Registra a Blueprint da rota de metricas do Prometheus
+metrics_bp = Blueprint("metrics", __name__)
+
+@metrics_bp.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
